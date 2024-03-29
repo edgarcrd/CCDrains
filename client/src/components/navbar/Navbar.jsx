53e1dbf9d009247f99ee5e3f4f8ec7c9db1ee0db
Edgar <edgarcrd@gmail.com>
2024-03-28 20:51:36 -0400

    Mobile Menu Draft

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,23 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import { RiMenuFill, RiCloseFill } from 'react-icons/ri';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GoPlus } from "react-icons/go";
+//import { RiMenuFill, iCloseFill } from "react-icons/ri";
 import logo from '../../assets/logo.png'
 
+const Menu = () => (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#about">About<GoPlus className="plus_pin"/></a></p>
+    <p><a href="#services">Services<GoPlus className="plus_pin"/></a></p>
+    <p><a href="#area">Service Area<GoPlus className="plus_pin"/></a></p>
+    <p><a href="#work">Recent Work</a></p>
+    <p><a href="#hiring">Now Hiring</a></p>
+  </>
+)
+
 const Navbar = () => {
+
+  const [ toggleMenu, setToggleMenu ] = useState(false);
+
+  const [ click, setClick ] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  const [ isOpen, setIsOpen ] = useState(false);
+
   return (
     <div className='_navbar'>
-      <img src={logo} alt='logo'/>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#area">Service Area</a></li>
-        <li><a href="#work">Recent Work</a></li>
-        <li><a href="#hiring">Now Hiring</a></li>
-        <li><a href="#contact">Contact Us</a></li>
-      </ul>
+      <div className='_navbar-links'>
+        <div className = '_navbar-links_logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+        <div className='_navbar-links_container'>
+          <Menu/>
+          {/* <p className="nav-button"><a href="#contact">Contact Us</a></p> */}
+        </div>
+        <div className='_navbar-contact'>
+          <button type='button'>Contact Us</button>
+        </div>
+        <div className='_navbar-menu'>
+          {toggleMenu
+            ? <RiCloseFill color="#71BF44" size={27} onClick={() => setToggleMenu(false)}/>
+            : <GiHamburgerMenu color="#71BF44" size={27} onClick={() => setToggleMenu(true)}/>
+          }
+          {toggleMenu && (
+            <div className='_navbar-menu_container scale-up-center'>
+              <div className='_navbar-menu_container-links'>
+                <Menu/>
+                <div className='_navbar-menu_container-links-contact'>
+                  <button type='button'>Contact Us</button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
--- client/src/containers/header/Header.jsx
+++ client/src/containers/header/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import './header.css'
 
+import { SlLocationPin } from "react-icons/sl";
+import { FaLocationDot } from "react-icons/fa6";
+import { BsTelephone, BsFillTelephoneFill } from "react-icons/bs";
+import { IoIosMail } from "react-icons/io";
+import { GrMail } from "react-icons/gr";
+import { TbMailFilled } from "react-icons/tb";
+
+
+
 const Header = () => {
   return (
     <div className='_header'>
       <div className='info__links'>
-        <div>
-          <a href="#">230 East Eagle St</a>
+        <div className="text-left">
+          <a href="#"><FaLocationDot className="header_pin"/>  22 Messina Dr Unit R, Braintree, MA 02184</a>
         </div>
         <div className="text-center">
-          <a href="#">(000) 000-000</a>
+          <a href="#"><BsFillTelephoneFill className="header_pin"/>  Office: (781) 521-9306</a>
         </div>
         <div className="text-right">
-          <a href="#">ccdrains.gmail.com</a>
+          <a href="#"><TbMailFilled className="mail_pin"/>  info@ccdrains.com</a>
         </div>
       </div>
     </div>
